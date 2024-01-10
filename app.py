2e83593248378f9b8d8d79338ad6b9b9b737e62c
Guillaume Ruff <guiguiruff@gmail.com>
2024-01-10 13:04:51 +0100

    Modfied code and template to handle multiple image

--- app.py
+++ app.py
@@ -19,26 +19,28 @@ model = keras.models.load_model('model/bird_model.keras')
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'files' not in request.files:
             return jsonify({'error': 'No file part'})
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'})
-        if file:
+
+        files = request.files.getlist('files')
+        results = []
+
+        for file in files:
+            if file.filename == '':
+                results.append({'error': 'No selected file'})
+                continue
+
             image = Image.open(io.BytesIO(file.read()))
             image = image.resize((224, 224))  # Resize the image
             image = np.expand_dims(image, axis=0)
             prediction = model.predict(image)[0]
 
-            # Find the index of the highest prediction
+            # Process each file similar to before
             top_pred_index = np.argmax(prediction)
-
-            # Create a sorted list of all predictions
             all_predictions = [(i, prob) for i, prob in enumerate(prediction)]
             all_predictions.sort(key=lambda x: x[1], reverse=True)
 
-            # Prepare the response dictionary
-            response = {
+            image_result = {
                 'top_prediction': {
                     'label': labels_dict[top_pred_index],
                     'probability': round(prediction[top_pred_index] * 100, 3)
@@ -46,25 +48,24 @@ def upload_file():
                 'other_predictions': []
             }
 
-            # Initialize the sum of small probabilities
             sum_small_probs = 0
-
             for index, prob in all_predictions:
                 if index != top_pred_index:
                     if prob >= 0.0001:
-                        response['other_predictions'].append({
+                        image_result['other_predictions'].append({
                             'label': labels_dict[index],
                             'probability': round(prob * 100, 3)
                         })
                     else:
                         sum_small_probs += prob
 
-            # Add the sum of small probabilities as 'Misc'
             if sum_small_probs > 0:
-                response['other_predictions'].append({
-                    'label': 'Misc',
+                image_result['other_predictions'].append({
+                    'label': 'Other',
                     'probability': round(sum_small_probs * 100, 3)
                 })
 
-            return jsonify(response)
+            results.append(image_result)
+
+        return jsonify(results)
     return render_template('upload.html')
